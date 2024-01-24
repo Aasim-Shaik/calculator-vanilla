57af51038203a04bfdb6cccc040fc6162817f502
Aasim-Shaik <68710345+Aasim-Shaik@users.noreply.github.com>
2024-01-24 14:28:47 +0530

    feat: add sum, subtract, divide and multiply operations

---
+++ script.js
@@ -0,0 +1,27 @@
+function add(num1 , num2){
+    if(typeof num1 == 'number' && typeof num2 == 'number')
+        return num1 + num2;
+    else
+        return -1;
+}
+
+function substract(num1 , num2){
+    if(typeof num1 == 'number' && typeof num2 == 'number')
+        return num1 - num2;
+    else
+        return -1;
+}
+
+function multiply(num1 , num2){
+    if(typeof num1 == 'number' && typeof num2 == 'number')
+        return num1 * num2;
+    else
+        return -1;
+}
+
+function divide(num1 , num2){
+    if(typeof num1 == 'number' && typeof num2 == 'number')
+        return num1 / num2;
+    else
+        return -1;
+}
