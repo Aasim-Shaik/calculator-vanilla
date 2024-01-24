ec65eaf7ab2515512bb729433fceeae7008c581e
Aasim-Shaik <68710345+Aasim-Shaik@users.noreply.github.com>
2024-01-24 23:15:11 +0530

    feat: add html css design for calculator, js for dark/light mode

--- script.js
+++ script.js
@@ -25,3 +25,45 @@ function divide(num1 , num2){
     else
         return -1;
 }
+
+let themeButton = document.querySelector('#theme-button');
+themeButton.addEventListener('click' , (e) => {
+    let theme = document.body.dataset.themeMode;
+
+    if(theme == 'dark')
+        theme = 'light';
+    else if(theme == 'light')
+        theme = 'dark';
+
+        document.body.dataset.themeMode = theme;
+});
+
+
+/*
+let bt = document.querySelector('#button-container');
+bt.addEventListener('click' , (e) => {
+    console.log(e.target.id);
+})
+function calculate(num1 , num2 , operator){
+    let result;
+    switch (operator) {
+        case '+':
+            result=add(num1,num2);
+            break;
+        case '-':
+            result=substract(num1,num2);
+            break;
+        case '/':
+            result=divide(num1,num2);
+            break;
+        case '*':
+            result=multiply(num1,num2);
+            break;    
+        default:
+            result=-1;
+            break;
+    }
+
+    return result;
+}
+*/
