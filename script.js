18bd1283998236ac00f8567ef641d71012bebbe2
Aasim-Shaik <68710345+Aasim-Shaik@users.noreply.github.com>
2024-01-25 13:04:12 +0530

    feat: add functionality to calculator buttons

--- script.js
+++ script.js
@@ -1,29 +1,136 @@
-function add(num1 , num2){
-    if(typeof num1 == 'number' && typeof num2 == 'number')
-        return num1 + num2;
-    else
-        return -1;
+
+class Calculator {
+
+    #isDecimalExists = false;
+
+    #calulatorArr = [];
+
+    #operator ;
+
+    #operatorMap = {add:this.#add, substract:this.#substract, division:this.#divide, multiplication:this.#multiply, '':this.#doNothing};
+
+    constructor() {
+    }
+
+    calculate(){
+        if(this.#calulatorArr.length >= 2){
+            this.#calulatorArr[0] = this.#operatorMap[this.#operator](this.#calulatorArr[0],this.#calulatorArr[1]);
+            this.#calulatorArr.splice(1,1);
+        }
+    }
+
+    get getNumber() {
+        return this.#calulatorArr[0];
+    }
+    get getArray() {
+        return this.#calulatorArr;
+    }
+
+    clearArray() {
+        this.#calulatorArr = [];
+        this.#operator = '';
+    }
+
+    get operator() {
+        return this.#operator;
+    }
+
+    setOperator(id){
+        this.#operator = id;
+    }
+
+    get isDecimalExists(){
+        return this.#isDecimalExists;
+    }
+
+    setDecimalExists(bool){
+        this.#isDecimalExists = bool;
+    }
+
+    pushNumber(num) {
+        this.#calulatorArr.push(num);
+    }
+
+    #doNothing(num1,num2){
+        return;
+    }
+
+    #add(num1 , num2){
+        if(typeof num1 == 'number' && typeof num2 == 'number')
+            return num1 + num2;
+        else
+            return -1;
+    }
+    
+    #substract(num1 , num2){
+        if(typeof num1 == 'number' && typeof num2 == 'number')
+            return num1 - num2;
+        else
+            return -1;
+    }
+    
+    #multiply(num1 , num2){
+        if(typeof num1 == 'number' && typeof num2 == 'number')
+            return Math.round((num1 * num2)*10000)/10000;
+        else
+            return -1;
+    }
+    
+    #divide(num1 , num2){
+        if(typeof num1 == 'number' && typeof num2 == 'number')
+            return Math.round((num1 / num2)*10000)/10000;
+        else
+            return -1;
+    }
+
 }
 
-function substract(num1 , num2){
-    if(typeof num1 == 'number' && typeof num2 == 'number')
-        return num1 - num2;
-    else
-        return -1;
+let calc = new Calculator();
+
+function printToScreen(num){
+    let screen = document.querySelector('#screen');
+    let length = screen.dataset.length;
+    if(length >20){
+        screen.textContent = 'Too long Number!';
+        return false;
+    }
+    screen.dataset.length = ++length;
+    screen.textContent += num;
+    return true;
 }
 
-function multiply(num1 , num2){
-    if(typeof num1 == 'number' && typeof num2 == 'number')
-        return num1 * num2;
-    else
-        return -1;
+function clearScreen(){
+    let screen = document.querySelector('#screen');
+    screen.dataset.length = 0;
+    calc.setDecimalExists(false);
+    screen.textContent = '';
+
 }
 
-function divide(num1 , num2){
-    if(typeof num1 == 'number' && typeof num2 == 'number')
-        return num1 / num2;
+function backspaceScreen(){
+    let screen = document.querySelector('#screen');
+    let content = screen.textContent;
+    let size = content.length;
+
+    if(size == 0)
+        return false;
+
+    if(content[size-1] == '.')
+        calc.setDecimalExists(false);
+
+    content = content.slice(0,-1);
+    screen.textContent = content;
+    return true;
+}
+
+function pushToArray(num){
+    let number = +num;
+    if(!isNaN(number)){
+        calc.pushNumber(number);
+        return true;
+    }
     else
-        return -1;
+        return false;
 }
 
 let themeButton = document.querySelector('#theme-button');
@@ -38,32 +145,66 @@ themeButton.addEventListener('click' , (e) => {
         document.body.dataset.themeMode = theme;
 });
 
+let buttonContainer = document.querySelector('#button-container');
+buttonContainer.addEventListener('click' , (e) => {
+    let id = e.target.id;
+    let digit = +id.substr(-1);
 
-/*
-let bt = document.querySelector('#button-container');
-bt.addEventListener('click' , (e) => {
-    console.log(e.target.id);
-})
-function calculate(num1 , num2 , operator){
-    let result;
-    switch (operator) {
-        case '+':
-            result=add(num1,num2);
-            break;
-        case '-':
-            result=substract(num1,num2);
-            break;
-        case '/':
-            result=divide(num1,num2);
-            break;
-        case '*':
-            result=multiply(num1,num2);
-            break;    
-        default:
-            result=-1;
-            break;
+    if(id == 'clear'){
+        calc.clearArray();
+        clearScreen();
     }
 
-    return result;
-}
-*/
+    if(id == 'backspace')
+        backspaceScreen();
+
+    if(id == 'equal') {
+        let content = document.querySelector('#screen').textContent;
+        if(content != '' && calc.operator != ''){
+            clearScreen();
+            pushToArray(content)
+            calc.calculate()
+            calc.setOperator('');
+            printToScreen(calc.getNumber != undefined ? calc.getNumber : '');
+            calc.clearArray();
+    }
+
+        console.log(calc.arr);
+    }
+
+    if(id == 'decimal'){
+        let decimalState = calc.isDecimalExists;
+
+        if(!decimalState){
+            calc.setDecimalExists(true);
+            if(document.querySelector('#screen').textContent == '')
+                printToScreen('0.');
+            else
+                printToScreen('.');
+        }
+        else{
+            //do nothing
+        }
+    }
+
+    if(id.startsWith('num') && isNaN(digit) == false){
+        printToScreen(digit);
+    }
+
+    if( id == 'add' || id == 'substract' || id == 'division' || id == 'multiplication' ){
+        let content = document.querySelector('#screen').textContent;
+        if(content == ''){
+            calc.setOperator(id);
+        }
+
+        else if(content != ''){
+            pushToArray(content);
+            clearScreen();
+            calc.calculate();
+            calc.setOperator(id);
+        }
+
+    }
+
+
+});
