ed20b7dae1f96dace2f98fdfadc33d229e505866
Aasim-Shaik <68710345+Aasim-Shaik@users.noreply.github.com>
2024-01-26 07:55:31 +0530

    refactor: change input scope from global to local
    the input is changed from html element to a local class variable for
    processing numbers

--- script.js
+++ script.js
@@ -1,146 +1,111 @@
 
 class Calculator {
 
+    #screenValue = '';
+
+    #screenLength = 0 ;
+    
     #isDecimalExists = false;
-
+    
     #calulatorArr = [];
-
+    
     #operator ;
-
+    
     #operatorMap = {add:this.#add, substract:this.#substract, division:this.#divide, multiplication:this.#multiply, '':this.#doNothing};
-
+    
     constructor() {
+
     }
 
+    setScreenValue(num){
+        this.#screenValue = num;
+    }
+
+    get screenValue(){
+        return this.#screenValue;
+    }
+
+    setScreenLength(num) {
+        this.#screenLength = num;
+    }
+
+    get screenlength(){
+        return this.#screenLength;
+    }
+    
     calculate(){
         if(this.#calulatorArr.length >= 2){
             this.#calulatorArr[0] = this.#operatorMap[this.#operator](this.#calulatorArr[0],this.#calulatorArr[1]);
             this.#calulatorArr.splice(1,1);
         }
     }
-
+    
     get getNumber() {
         return this.#calulatorArr[0];
     }
     get getArray() {
         return this.#calulatorArr;
     }
-
+    
     clearArray() {
         this.#calulatorArr = [];
         this.#operator = '';
     }
-
+    
     get operator() {
         return this.#operator;
     }
-
+    
     setOperator(id){
         this.#operator = id;
     }
-
+    
     get isDecimalExists(){
         return this.#isDecimalExists;
     }
-
+    
     setDecimalExists(bool){
         this.#isDecimalExists = bool;
     }
-
+    
     pushNumber(num) {
         this.#calulatorArr.push(num);
     }
-
+    
     #doNothing(num1,num2){
         return;
     }
-
+    
     #add(num1 , num2){
         if(typeof num1 == 'number' && typeof num2 == 'number')
-            return num1 + num2;
-        else
-            return -1;
-    }
-    
-    #substract(num1 , num2){
-        if(typeof num1 == 'number' && typeof num2 == 'number')
-            return num1 - num2;
-        else
-            return -1;
-    }
-    
-    #multiply(num1 , num2){
-        if(typeof num1 == 'number' && typeof num2 == 'number')
-            return Math.round((num1 * num2)*10000)/10000;
-        else
-            return -1;
-    }
-    
-    #divide(num1 , num2){
-        if(typeof num2 == 'number' && num2 == 0)
-            return 'error';
-        else if(typeof num1 == 'number' && typeof num2 == 'number')
-            return Math.round((num1 / num2)*10000)/10000;
-        else
-            return -1;
-    }
-
-}
-
-let calc = new Calculator();
-
-function printToScreen(num){
-    let screen = document.querySelector('#screen');
-    let length = screen.dataset.length;
-    if(length >20){
-        screen.textContent = 'Too long Number!';
-        return false;
-    }
-    screen.dataset.length = ++length;
-    screen.textContent += num;
-    return true;
-}
-
-function printOperatorToScreen(id){
-    let operatorScreen = document.querySelector('#current-operator');
-    const idToOperator = {add:'+', substract:'-', division:'/', multiplication:'*', '':''};
-    operatorScreen.textContent = idToOperator[id];
-}
-
-function clearScreen(){
-    let currOperator = document.querySelector('#current-operator');
-    currOperator.textContent = '';
-    let screen = document.querySelector('#screen');
-    screen.dataset.length = 0;
-    calc.setDecimalExists(false);
-    screen.textContent = '';
-
-}
-
-function backspaceScreen(){
-    let screen = document.querySelector('#screen');
-    let content = screen.textContent;
-    let size = content.length;
-
-    if(size == 0)
-        return false;
-
-    if(content[size-1] == '.')
-        calc.setDecimalExists(false);
-
-    content = content.slice(0,-1);
-    screen.textContent = content;
-    return true;
-}
-
-function pushToArray(num){
-    let number = +num;
-    if(!isNaN(number)){
-        calc.pushNumber(number);
-        return true;
-    }
+        return num1 + num2;
     else
-        return false;
+    return -1;
+}
+
+#substract(num1 , num2){
+    if(typeof num1 == 'number' && typeof num2 == 'number')
+    return num1 - num2;
+else
+return -1;
+}
+
+#multiply(num1 , num2){
+    if(typeof num1 == 'number' && typeof num2 == 'number')
+    return Math.round((num1 * num2)*10000)/10000;
+else
+return -1;
+}
+
+#divide(num1 , num2){
+    if(typeof num2 == 'number' && num2 == 0)
+    return 'error';
+else if(typeof num1 == 'number' && typeof num2 == 'number')
+return Math.round((num1 / num2)*10000)/10000;
+else
+return -1;
+}
+
 }
 
 let themeButton = document.querySelector('#theme-button');
@@ -155,66 +120,141 @@ themeButton.addEventListener('click' , (e) => {
         document.body.dataset.themeMode = theme;
 });
 
-let buttonContainer = document.querySelector('#button-container');
-buttonContainer.addEventListener('click' , (e) => {
-    let id = e.target.id;
-    let digit = +id.substr(-1);
+addEventListener('load' , (e) => {
+    let calc = new Calculator();
+    
+    let buttonContainer = document.querySelector('#button-container');
+    buttonContainer.addEventListener('click' , (e) => {
+        calculatorLogic(e,calc);
+    });
 
-    if(id == 'clear'){
-        calc.clearArray();
-        clearScreen();
-    }
+    function calculatorLogic(e, calc) {
 
-    if(id == 'backspace')
-        backspaceScreen();
-
-    if(id == 'equal') {
-        let content = document.querySelector('#screen').textContent;
-        if(content != '' && calc.operator != ''){
-            clearScreen();
-            pushToArray(content)
-            calc.calculate()
-            calc.setOperator('');
-            printToScreen(calc.getNumber != undefined ? calc.getNumber : '');
-            calc.clearArray();
-    }
-
-        console.log(calc.getArray);
-    }
-
-    if(id == 'decimal'){
-        let decimalState = calc.isDecimalExists;
-
-        if(!decimalState){
-            calc.setDecimalExists(true);
-            if(document.querySelector('#screen').textContent == '')
-                printToScreen('0.');
+        function printToScreen(num){
+            let screen = document.querySelector('#screen');
+        
+            let length = calc.screenValue.length;
+        
+            if(length >17){
+                screen.textContent = 'Too long Number!';
+                return false;
+            }
+        
+            calc.setScreenValue(calc.screenValue+num);
+            calc.setScreenLength(++length);
+        
+            screen.textContent = calc.screenValue;
+            return true;
+        }
+        
+        function printOperatorToScreen(id){
+            let operatorScreen = document.querySelector('#current-operator');
+            const idToOperator = {add:'+', substract:'-', division:'/', multiplication:'*', '':''};
+            operatorScreen.textContent = idToOperator[id];
+        }
+        
+        function clearScreen(){
+            //clear the HTML screen
+            let screen = document.querySelector('#screen');
+            screen.textContent = '';
+        
+            //clear the class variable
+            calc.setScreenLength(0);
+            calc.setScreenValue('');
+            calc.setDecimalExists(false);
+        }
+        
+        function backspaceScreen(){
+            let screen = document.querySelector('#screen');
+            
+            let content = calc.screenValue;
+            let size = content.length;
+        
+            if(size == 0)
+                return false;
+        
+            if(content[size-1] == '.')
+                calc.setDecimalExists(false);
+        
+            content = content.slice(0,-1);
+            calc.setScreenValue(content);
+            calc.setScreenLength(--size);
+            screen.textContent = calc.screenValue;
+            return true;
+        }
+        
+        function pushToArray(numString){
+            let number = +numString;
+            if(!isNaN(number)){
+                calc.pushNumber(number);
+                return true;
+            }
             else
-                printToScreen('.');
+                return false;
         }
-        else{
-            //do nothing
+        
+            let id = e.target.id;
+            let digit = +id.substr(-1);
+        
+            if(id == 'clear'){
+                calc.clearArray();
+                clearScreen();
+                let currOperator = document.querySelector('#current-operator');
+                currOperator.textContent = '';
+            }
+        
+            if(id == 'backspace')
+                backspaceScreen();
+        
+            if(id == 'equal') {
+                let content = calc.screenValue;
+                if(content != '' && calc.operator != ''){
+                    clearScreen();
+                    pushToArray(content)
+                    calc.calculate()
+                    calc.setOperator('');
+                    printToScreen(calc.getNumber != undefined ? calc.getNumber : '');
+                    calc.clearArray();
+            }
+        
+            }
+        
+            if(id == 'decimal'){
+                let decimalState = calc.isDecimalExists;
+        
+                if(!decimalState){
+                    calc.setDecimalExists(true);
+                    if(calc.screenValue == ''){
+                        printToScreen('0.');
+                    }
+                    else
+                        printToScreen('.');
+                }
+                else{
+                    //do nothing
+                }
+            }
+        
+            if(id.startsWith('num') && isNaN(digit) == false){
+                printToScreen(digit);
+            }
+        
+            if( id == 'add' || id == 'substract' || id == 'division' || id == 'multiplication' ){
+                printOperatorToScreen(id);
+                let content = calc.screenValue;
+                if(content == ''){
+                    calc.setOperator(id);
+                }
+        
+                else if(content != ''){
+                    pushToArray(content);
+                    clearScreen();
+                    calc.calculate();
+                    calc.setOperator(id);
+                }
+        
+            }
+        
         }
-    }
-
-    if(id.startsWith('num') && isNaN(digit) == false){
-        printToScreen(digit);
-    }
-
-    if( id == 'add' || id == 'substract' || id == 'division' || id == 'multiplication' ){
-        printOperatorToScreen(id);
-        let content = document.querySelector('#screen').textContent;
-        if(content == ''){
-            calc.setOperator(id);
-        }
-
-        else if(content != ''){
-            pushToArray(content);
-            clearScreen();
-            calc.calculate();
-            calc.setOperator(id);
-        }
-
-    }
-
+    
 });
